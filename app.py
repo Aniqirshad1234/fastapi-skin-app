592ec550f0186452d6eddcb4fbfef4b0170b753f
Aniqirshad1234 <muhammadammarali02@gmail.com>
2025-07-15 01:46:19 +0500

    Update app.py

--- app.py
+++ app.py
@@ -336,3 +336,7 @@ async def predict(request: Request, file: UploadFile = File(...)):
 
 # Start DB
 init_db()
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("app:app", host="0.0.0.0", port=port)
+
